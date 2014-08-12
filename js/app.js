a0af83e85c5fe4da8e9ce2f283f401221f9a0678
Jenny Nilsson <jenny.nilsson@jayway.com>
2014-08-12 14:02:02 +0200

    Working code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
-/*!
-** Example App
-** Licensed under the Apache License v2.0
-** http://www.apache.org/licenses/LICENSE-2.0
-** Built by Jay Kanakiya ( @techiejayk )
-**/
+
 "use strict";
 var App = angular.module("example",[]);
-App.controller("ExmpCtrl",function ($scope) {
-//
+
+App.controller("ExmpCtrl",function ($scope, $http) {
+
+	$scope.todos = [
+	{ taskName : "Write an Angular js Tutorial for Todo-List" , isDone : false },
+	{ taskName : "Update jquer.in" , isDone : false },
+	{ taskName : "Create a brand-new Resume" , isDone : false }
+	];
+	
+	
+	$getResult($scope, $http);
+	
+	$http({method: 'GET', 
+	url: 'http://www.corsproxy.com/api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?',  
+	params : { tags: "mount rainier", tagmode: "any", format: "json" }, 
+	headers: {'Accept': 'application/json'} }).
+	success(function(data, status, headers, config) {
+		$scope.images =  data.items;
+	}).
+	error(function(data, status, headers, config) {
+		
+	});
+	
 });
