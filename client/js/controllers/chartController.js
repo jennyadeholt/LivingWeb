b53c6aac131cc1a7e22753b238ebd57382078a01
jennynilsson <jenny.nilsson@jayway.com>
2015-12-14 14:54:35 +0100

    Update UI

--- client/js/controllers/chartController.js
+++ client/js/controllers/chartController.js
@@ -1,73 +1,73 @@
-
 var libLoaded = false;
 
 var options = {
-	title: 'Kvadratmeterpris / månad',
-	height: 599,
-	width: 700,
-	legend: {position: 'right'},
-	titlePosition: 'top',
-	orientation: 'vertical',
-	annotations: {
-		alwaysOutside: true
-	},
-	seriesType: "bars",
-	series: {
-		0: { color: '#04567C'},
-		1: { color: '#68cbfa'},
-		2: {
-			type: "line",
-			color: 'red'
-		}
-	}
+    title: 'Kvadratmeterpris / månad',
+    height: 599,
+    width: 700,
+    legend: {position: 'right'},
+    titlePosition: 'top',
+    orientation: 'vertical',
+    annotations: {
+        alwaysOutside: true
+    },
+    seriesType: "bars",
+    series: {
+        0: {color: '#04567C'},
+        1: {color: '#68cbfa'},
+        2: {
+            type: "line",
+            color: 'red'
+        }
+    }
 };
 
-google.load("visualization", "1", {packages:["corechart"]});
+google.load("visualization", "1", {packages: ["corechart"]});
 google.setOnLoadCallback(function () {
-	libLoaded = true;
+    libLoaded = true;
 });
 
 angular.module('livingWebApp').controller('ChartCtrl',
-function ChartController($scope, ProfitService, $filter) {
+    function ChartController($scope, ProfitService, $filter) {
 
-	$scope.$watch(
-		function() {
-			return $scope.profits;
-		},
-		function(scope) {
-			drawChart();
-		}
-	);
+        $scope.$watch(
+            function () {
+                return $scope.profits;
+            },
+            function (scope) {
+                drawChart();
+            }
+        );
 
-	$scope.init = function () {}
+        $scope.init = function () {
+        }
 
-		/*{
-		calc: getValueAt.bind(undefined, 1),
-		sourceColumn: 1,
-		type: "string",
-		role: "annotation"
-	}*/
+        /*{
+         calc: getValueAt.bind(undefined, 1),
+         sourceColumn: 1,
+         type: "string",
+         role: "annotation"
+         }*/
 
-function drawChart() {
-	var objects = $scope.profits;
-	if (objects && objects.length > 0) {
-		var data = google.visualization.arrayToDataTable(ProfitService.getAveragePerMonths(objects));
+        function drawChart() {
+            var objects = $scope.profits;
+            if (objects && objects.length > 0) {
+                var data = google.visualization.arrayToDataTable(ProfitService.getAveragePerMonths(objects));
 
-		var view = new google.visualization.DataView(data);
+                var view = new google.visualization.DataView(data);
 
-		view.setColumns([0, 1, {
-			calc: getValueAt.bind(undefined, 1),
-			sourceColumn: 1,
-			type: "string",
-			role: "annotation"
-		}, 2, 3]);
+                view.setColumns([0, 1, {
+                    calc: getValueAt.bind(undefined, 1),
+                    sourceColumn: 1,
+                    type: "string",
+                    role: "annotation"
+                }, 2, 3]);
 
-		var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
-		chart.draw(view, options);
-	}
-}
+                var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+                chart.draw(view, options);
+            }
+        }
 
-function getValueAt(column, dataTable, row) {
-	return $filter('kvmprice')(dataTable.getFormattedValue(row, column)) + $filter('kvmprice')(dataTable.getFormattedValue(row, column + 1));
-}
-});
+        function getValueAt(column, dataTable, row) {
+            return $filter('kvmprice')(dataTable.getFormattedValue(row, column)) + $filter('kvmprice')(dataTable.getFormattedValue(row, column + 1));
+        }
+    });
